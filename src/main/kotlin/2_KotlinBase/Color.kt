bc252ca0df52c68c4de90e63a92ab8a9f0fd8813
go-coding1 <rhtjrwnszyzy@gmail.com>
2024-01-07 21:59:38 +0900

    page 83 스마트 캐스트 공부시작해야함.

--- src/main/kotlin/2_KotlinBase/Color.kt
+++ src/main/kotlin/2_KotlinBase/Color.kt
@@ -24,3 +24,21 @@ fun getWarmth(color: Color) = when(color) {
     Color.GREEN -> "neutral"
     Color.BLUE, Color.INDIGO, Color.VIOLET -> "cold"
 }
+
+fun mix(c1: Color, c2: Color) =
+    when (setOf(c1, c2)){
+        setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
+        setOf(Color.YELLOW, Color.BLUE) -> Color.GREEN
+        setOf(Color.BLUE, Color.VIOLET) -> Color.INDIGO
+        else -> throw Exception("Dirty Color")
+    }
+
+fun mixOptimized(c1:Color,c2: Color) =
+    when {
+        (c1 == Color.RED && c2 ==Color.YELLOW) || (c1 == Color.YELLOW && c2 == Color.RED) -> Color.ORANGE
+        else -> throw Exception("Dirty Color")
+    }
+fun main(array: Array<String>){
+    print(mix(Color.BLUE, Color.YELLOW))
+    print(mixOptimized(Color.BLUE, Color.YELLOW))
+}
