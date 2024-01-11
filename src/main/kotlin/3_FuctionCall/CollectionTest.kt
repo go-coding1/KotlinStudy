138a73b2a266580fe2aa398dc51f6be19042780b
go-coding1 <rhtjrwnszyzy@gmail.com>
2024-01-11 21:24:06 +0900

    page 107 함수 호출 쉽게 만들기, list,set,map은 자바에서 쓰던 Collection 과 똑같은거 사용, 그저 기능만 추가

--- src/main/kotlin/3_FuctionCall/CollectionTest.kt
+++ src/main/kotlin/3_FuctionCall/CollectionTest.kt
@@ -1,2 +1,32 @@
 package `3_FuctionCall`
 
+fun main(){
+    val set = hashSetOf(1,7,23) //숫자로 이뤄진 집합
+
+    val list = arrayListOf(1,7,53)  //숫자로 이뤄진 리스트
+    val map = hashMapOf(1 to "one", 7 to "seven", 53 to "fifty-three")  //to는 일반 함수
+
+    println(set.javaClass)
+    println(list.javaClass)
+    println(map.javaClass)
+
+    println(list.last())
+    println(list.max())
+
+    println(joinToString(list, "; ","(",")"))
+}
+
+fun <T> joinToString(
+    collection: Collection<T>,
+    separator: String,
+    prefix: String,
+    postfix: String
+): String {
+    val result = StringBuilder(prefix)
+    for((index, element) in collection.withIndex()){
+        if(index>0) result.append(separator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
